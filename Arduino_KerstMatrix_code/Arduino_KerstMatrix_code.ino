db4ba5fbb4b27c3f813321e1661dadcb347d0ce9
Stijnovich <svlierop@xs4all.nl>
2016-12-16 15:28:19 +0100

    arduino matrix controller
    ontvangt berichten, verwerkt deze nog niet

--- Arduino_KerstMatrix_code/Arduino_KerstMatrix_code.ino
+++ Arduino_KerstMatrix_code/Arduino_KerstMatrix_code.ino
@@ -1,35 +1,99 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+#include <PubSubClient.h>
 #include "FastLED.h"
 
 #define NUM_LEDS 144
-
 #define DATA_PIN 5
+#define BRIGHTNESS  255
 
-#define BRIGHTNESS  64
+char ssid[] = "12connect";
+int wifiStatus = WL_IDLE_STATUS;
+WiFiClient client;
+IPAddress server(145, 85, 4, 91);
+PubSubClient mqttClient(client);
+
+char mqttUser[] = "i339322_timojansen";
+char mqttPass[] = "Xjr1MGsdYEKtJ5";
+String mqttClientId = "arduino-";
+String subscribeTopic = "private/i339322_timojansen/#";
 
 CRGB leds[NUM_LEDS];
-
-int bitMap[NUM_LEDS] = {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-                        1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+int bitMap[NUM_LEDS] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+                        1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 1,
+                        1, 2, 3, 4, 4, 4, 4, 4, 4, 3, 2, 1,
+                        1, 2, 3, 4, 5, 5, 5, 5, 4, 3, 2, 1,
+                        1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 1,
+                        1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 1,
+                        1, 2, 3, 4, 5, 5, 5, 5, 4, 3, 2, 1,
+                        1, 2, 3, 4, 4, 4, 4, 4, 4, 3, 2, 1,
+                        1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 1,
+                        1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                        };
 
 void setup() {
-  delay(2000);
+  delay(1000);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   FastLED.setBrightness(  BRIGHTNESS );
+  updateMatrix();
+  Serial.begin(57600);
+  Serial.println("Arduino C# chat starting..");
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  mqttClientId += macToStr(mac);
+  Serial.println("Client ID: " + mqttClientId);
+  connectWifi();
+  if ( wifiStatus != WL_CONNECTED) {
+    Serial.println("Could not connect to Wifi");
+    while (true);
+  }
+  else {
+    Serial.println("Connected to Wifi");
+    delay(5000);
+    Serial.println(WiFi.localIP());
+  }
+  mqttClient.setServer(server, 1883);
+  mqttClient.setCallback(callback);
+  connectMqtt();
 }
 
 void loop() {
-  updateMatrix();
+  if (wifiStatus != WL_CONNECTED)
+    connectWifi();
+
+  while (!mqttClient.connected())
+    connectMqtt();
+
+  mqttClient.loop();
+}
+
+void connectWifi() {
+  Serial.println("Initializing Wifi...");
+  wifiStatus = WiFi.begin(ssid);
+}
+
+void connectMqtt() {
+  if (!mqttClient.connect((char*) mqttClientId.c_str(), mqttUser, mqttPass))
+  {
+    Serial.println("ERROR: Cannot connect to MQTT server! Error code: " + String(mqttClient.state()));
+  }
+  else
+  {
+    Serial.println("Connected to MQTT");
+    if (mqttClient.subscribe((char *)subscribeTopic.c_str())) {
+      Serial.println("Subscribed to topic: " + subscribeTopic);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int len) {
+  Serial.println("MQTT topic: " + String(topic));
+  String payloadString = String((char*)payload);
+  Serial.println(payloadString);
 }
 
 void updateMatrix() {
@@ -67,4 +131,13 @@ void updateMatrix() {
   }
 
   FastLED.show();
+}
+
+String macToStr(const uint8_t* mac)
+{
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+  }
+  return result;
 }
